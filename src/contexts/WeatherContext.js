7a16b5c5f8f37268351c9a0c550a3de3a9b2ab20
姜大巍 <davy820ramsey@gmail.com>
2020-04-06 22:05:24 +1200

    update using js to get ip address link to https

--- src/contexts/WeatherContext.js
+++ src/contexts/WeatherContext.js
@@ -18,13 +18,13 @@ const WeatherContext = createContext();
 const WeatherContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(weatherReducer, initState);
 
-  const IP_API_URL = "http://ip-api.com/json";
+  const IP_API_URL = "https://ipapi.co/json/";
   const WEATHER_API_URL =
     "https://api.openweathermap.org/data/2.5/weather?APPID=3a58f9dd1f1515ec0dba25894ccea202&q=";
 
   const getWeather = useCallback(() => {
     get(IP_API_URL).then(res => {
-      let location = res.city + "," + res.countryCode;
+      let location = res.city + "," + res.country;
       get(WEATHER_API_URL + location).then(result => {
         try {
           dispatch({ type: GET_WEATHER_SUCCESS, data: result });
