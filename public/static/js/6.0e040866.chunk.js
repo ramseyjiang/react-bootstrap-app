3d054ef72aa0008db79bd024885e67978941f701
姜大巍 <davy820ramsey@gmail.com>
2020-04-07 11:01:08 +1200

    optimise movie context, add an default search

--- public/static/js/6.0e040866.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact-bootstrap-app"]=this["webpackJsonpreact-bootstrap-app"]||[]).push([[6],{77:function(t,o,n){"use strict";n.r(o);var a=n(0),e=n.n(a);o.default=function(){return console.log("Not found"),e.a.createElement("h1",null,"Not found")}}}]);
-//# sourceMappingURL=6.0e040866.chunk.js.map
--- src/components/movies/MovieSearch.jsx
+++ src/components/movies/MovieSearch.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Form, Button, Row, Col } from "react-bootstrap";
 import { useMovieContext } from "../../contexts/MovieContext";
 
@@ -6,12 +6,21 @@ const Search = () => {
   const { movieApi } = useMovieContext();
   const [searchValue, setSearchValue] = useState("");
   const MOVIE_API_URL = "https://www.omdbapi.com/?apikey=f91c3148&s=";
+  const DEFAULT_SEARCH = "Fast&Furious";
 
   const handleSearchChange = e => {
     setSearchValue(e.target.value);
   }
 
-  const handleSearch = () => {
+  // movieApi.searchMovie(MOVIE_API_URL + DEFAULT_SEARCH);
+  useEffect(()=>{
+    if(searchValue === '') {
+      movieApi.searchMovie(MOVIE_API_URL + DEFAULT_SEARCH)
+    } 
+  },[]);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
     movieApi.loading();
     if(searchValue !== '') {
       movieApi.searchMovie(MOVIE_API_URL + searchValue)
@@ -25,10 +34,10 @@ const Search = () => {
       <Form>
         <Form.Group as={Row}>
           <Col sm={8}>
-            <Form.Control type="text" placeholder="Input movie title" onChange={handleSearchChange} value={searchValue}/>
+            <Form.Control type="text" placeholder="Input movie title, for example: Fast&Furious" onChange={handleSearchChange} value={searchValue}/>
           </Col>
           <Col sm={4}>
-            <Button variant="primary" type="button" size="sm" onClick={handleSearch}>Search</Button>{' '}
+            <Button variant="primary" type="submit" size="sm" onClick={handleSearch}>Search</Button>{' '}
           </Col>
         </Form.Group>
       </Form>
