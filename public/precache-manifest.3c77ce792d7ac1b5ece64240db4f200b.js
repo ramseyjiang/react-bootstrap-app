83e6c789ae872098fbb392ef6f6fc0532c761232
姜大巍 <davy820ramsey@gmail.com>
2020-04-06 17:25:02 +1200

    try again

---
+++ public/precache-manifest.3c77ce792d7ac1b5ece64240db4f200b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a77e686a474308f44a38ae9c57ce2ba1",
+    "url": "/react-bootstrap-app/index.html"
+  },
+  {
+    "revision": "91785c4eaaaa4eef6729",
+    "url": "/react-bootstrap-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "8ba32908bda704e2fb27",
+    "url": "/react-bootstrap-app/static/css/main.1ea1dd26.chunk.css"
+  },
+  {
+    "revision": "91785c4eaaaa4eef6729",
+    "url": "/react-bootstrap-app/static/js/2.d96e5e6c.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/react-bootstrap-app/static/js/2.d96e5e6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db6334034d1ea708efc7",
+    "url": "/react-bootstrap-app/static/js/3.cd997084.chunk.js"
+  },
+  {
+    "revision": "948347134b1bb488b84e",
+    "url": "/react-bootstrap-app/static/js/4.1f4166d4.chunk.js"
+  },
+  {
+    "revision": "63194266887f9acf055a",
+    "url": "/react-bootstrap-app/static/js/5.e49c1f18.chunk.js"
+  },
+  {
+    "revision": "e20fc5fb64f2acfb4ec6",
+    "url": "/react-bootstrap-app/static/js/6.0e040866.chunk.js"
+  },
+  {
+    "revision": "8ba32908bda704e2fb27",
+    "url": "/react-bootstrap-app/static/js/main.bab262a8.chunk.js"
+  },
+  {
+    "revision": "3bedca8162bbc062ed63",
+    "url": "/react-bootstrap-app/static/js/runtime-main.93ddabe6.js"
+  }
+]);
