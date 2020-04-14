34cf7618c02f6751e851b85d3d350ae3e01e9613
姜大巍 <davy820ramsey@gmail.com>
2020-04-14 14:58:50 +1200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-bootstrap-app/precache-manifest.7ce452a8f779f7fac7bc0ad1576882e5.js"
+  "/react-bootstrap-app/precache-manifest.663bcd43249361193509ca974bed7956.js"
 );
 
 self.addEventListener('message', (event) => {
