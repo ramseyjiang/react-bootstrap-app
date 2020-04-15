f85c6e1c495d3bad8bcd9d7b0eac3b1cbb374ca6
姜大巍 <davy820ramsey@gmail.com>
2020-04-15 22:15:11 +1200

    update profile

--- src/components/profile/ShowProfile.jsx
+++ src/components/profile/ShowProfile.jsx
@@ -26,7 +26,7 @@ const ShowProfile = () => {
           <div className="col-9">
             <span>I am Ramsey Jiang, looking for a role as fullstack or backend or react developer.</span>
             <p>I am a New Zealand Permanent Resident and I have over 10 years experience as a fullstack developer. I Enjoy jogging and fishing.</p>
-            <p>Good at by following techs, such as PHP, Laravel, React, VueJS, JQuery, MySQL, Mongo, Ajax, HTML/HTML5, CSS/CSS3, Github, Docker, NodeJs, Python.</p>
+            <p>Good at by following techs, such as PHP, Zend, WordPress,Silver Stripe, Laravel, Synfomy, React, VueJS, JQuery, MySQL, Mongo, Memcache, Redis, Ajax, HTML/HTML5, CSS/CSS3, Github, Docker, NodeJs, Python, and so on.</p>
             
           </div>
           <div className="col-12">
