8db76ccff020fd7ddd7279ee4020bd06e24c7dde
姜大巍 <davy820ramsey@gmail.com>
2020-04-15 20:29:44 +1200

    update timeline descs

--- src/components/profile/Timeline.jsx
+++ src/components/profile/Timeline.jsx
@@ -3,7 +3,7 @@ import { Container } from "react-bootstrap";
 
 export default function TimeLine() {
   const obj = {
-    Home: {pos: "timeline-container left", content: 'It will show the author profile and the website guide timeline.'},
+    Home: {pos: "timeline-container left", content: 'It will show the current location weather info and author profile and the website guide timeline.'},
     Coins: {pos: "timeline-container right", content: 'It will show BTC, LTC and ETH and several exchange rates with USA dollar.'},
     Dogs: {pos: "timeline-container left", content: 'It is an infinite scroll page to show dogs pics from a third party api.'},
     Movies: {pos: "timeline-container right", content: 'The default search is <<Fast & Furious>>, it will send a search request to a third party API.'},
